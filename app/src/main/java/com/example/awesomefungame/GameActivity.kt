7e66ae60ecfe6a5d4e128e343ecb67d2c3ea1ff6
Brendon Marques <4thmarques@gmail.com>
2021-07-06 09:07:05 -0600

    Updates

--- app/src/main/java/com/example/awesomefungame/GameActivity.kt
+++ app/src/main/java/com/example/awesomefungame/GameActivity.kt
@@ -10,6 +10,18 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class GameActivity : AppCompatActivity() {
+    // This creates a random sequence that will be used as ID for the doc in the database
+    private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    private val playerKey = (1..15)
+        .map { _ -> kotlin.random.Random.nextInt(0, charPool.size) }
+        .map(charPool::get)
+        .joinToString("");
+
+    private lateinit var lobby: String
+
+    private val db = Firebase.firestore
+    private lateinit var database: AppDatabase
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +29,14 @@ class GameActivity : AppCompatActivity() {
 
         // Initiate player instance
         val playerName = intent.getStringExtra("name")
-        val lobby = intent.getStringExtra("lobby")
+        lobby = intent.getStringExtra("lobby").toString()
         val player = playerName?.let { Players(it, 0.0) }
-
-        // This creates a random sequence that will be used as ID for the doc in the database
-        val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        val playerKey = (1..10)
-            .map { _ -> kotlin.random.Random.nextInt(0, charPool.size) }
-            .map(charPool::get)
-            .joinToString("");
 
         // Lobby data
         val lobbyPlayers = LobbyData()
 
         // Initiate database instance
-        val db = Firebase.firestore
-        val database = AppDatabase(GameActivity())
+        database = AppDatabase(this)
 
         // Add new player to Lobby database
         database.addPlayerInLobby(db, player, lobby, playerKey)
@@ -61,6 +65,8 @@ class GameActivity : AppCompatActivity() {
 
     }
 
+    // This function is called whenever there is a change to the database, and it updates the adapter that
+    // shows the people in the lobby.
     fun setUpScoreData(lobbyData: LobbyData, adapter: ArrayAdapter<Any>, lvPlayers: ListView) {
         adapter.clear()
         lobbyData.players.map { x ->
@@ -68,5 +74,11 @@ class GameActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         }
         lvPlayers.adapter = adapter
+    }
+
+    // Remove player from the database if they
+    override fun onDestroy() {
+        super.onDestroy()
+        database.deletePlayerInLobby(db, playerKey, lobby)
     }
 }
